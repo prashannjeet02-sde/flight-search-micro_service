48378affe101a16463c20a6551a0c24b668367b2
prashannjeetHexan <prasannjeet.p@hexantech.com>
2026-03-28 15:37:41 +0530

    Added filters for flight

--- src/repositories/flights.repo.js
+++ src/repositories/flights.repo.js
@@ -1,6 +1,31 @@
 const { flights } = require("../models/index");
+const { Op } = require("sequelize");
 
 class FlightRepository {
+  #createFilter(data) {
+    let options = {};
+
+    if (data.source) {
+      options.departureAirportId = data.source;
+    }
+
+    if (data.destination) {
+      options.arrivalAirportId = data.destination;
+    }
+
+    if (data.minPrice || data.maxPrice) {
+      options.price = {};
+
+      if (data.minPrice) {
+        options.price[Op.gte] = data.minPrice;
+      }
+
+      if (data.maxPrice) {
+        options.price[Op.lte] = data.maxPrice;
+      }
+    }
+  }
+
   async RepoCreateFlight(data) {
     const flight = await flights.create(data);
     return flight;
@@ -9,6 +34,14 @@ class FlightRepository {
   async RepoGetFlight(flightId) {
     const getFlight = await flights.findByPk(flightId);
     return getFlight;
+  }
+
+  async RepoGetAllFlights(filter) {
+    const flightFilters = this.#createFilter(filter);
+    const allFlights = await flights.findAll({
+      where: flightFilters,
+    });
+    return allFlights;
   }
 }
 
